868e7699a22b87dcaec6b5eb1fbf47c1387eb893
tat cheng chiam <chiamcheng@gmail.com>
2020-02-28 14:52:29 +1000

    fetch a list of data and query a single data from apollo client + react hook. basic mutation from client

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,10 +1,94 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, {Fragment} from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-interface LaunchesProps extends RouteComponentProps {}
+import {LaunchTile, Header, Button, Loading} from '../components';
+import {RouteComponentProps} from '@reach/router';
+
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+
+interface LaunchesProps extends RouteComponentProps {
+}
+
+/*
+We define a GraphQL fragment by giving it a name (LaunchTile) and defining it on a type on our schema (Launch).
+The name we give our fragment can be anything, but the type must correspond to a type in our schema.
+ */
+const GET_LAUNCHES = gql`
+    query launchList($after:String){
+        launches(after: $after){
+            cursor
+            hasMore
+            launches{
+                ...LaunchTile
+            }
+        }
+    }
+`;
+
+export const LAUNCH_TILE_DATA = gql`
+    fragment LaunchTile on Launch{
+        id
+        isBooked
+        rocket {
+            id
+            name
+        }
+        mission {
+            name
+            missionPatch
+        }
+    }
+`
+
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+    const {
+        data, loading, error, fetchMore
+    } = useQuery<GetLaunchListTypes.GetLaunchList,
+        GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES);
+
+
+    if (loading) return <Loading/>;
+    if (error) return <p>ERROR</p>;
+    if (!data) return <p>Not found</p>;
+
+    return (<Fragment>
+        <Header/>
+        {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+            <LaunchTile key={launch.id} launch={launch}/>
+        ))}
+        {data.launches &&
+        data.launches.hasMore && (
+            <Button
+                onClick={() =>
+                    fetchMore({
+                        variables: {
+                            after: data.launches.cursor,
+                        },
+                        updateQuery: (prev, {fetchMoreResult, ...rest}) => {
+                            if (!fetchMoreResult) return prev;
+                            return {
+                                ...fetchMoreResult,
+                                launches: {
+                                    ...fetchMoreResult.launches,
+                                    launches: [
+                                        ...prev.launches.launches,
+                                        ...fetchMoreResult.launches.launches,
+                                    ],
+                                },
+                            };
+                        },
+                    })
+                }
+            >
+                Load More
+            </Button>
+        )
+        }
+    </Fragment>)
 }
 
 export default Launches;
