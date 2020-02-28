bc25bac15bde463915813dbfcf61cc9064906228
tat cheng chiam <chiamcheng@gmail.com>
2020-02-28 15:57:21 +1000

    virtual field from client and localState query

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -3,16 +3,30 @@ import gql from 'graphql-tag';
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {HttpLink} from "apollo-link-http";
-import {ApolloProvider} from "@apollo/react-hooks";
+import {ApolloProvider, useQuery} from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
 import injectStyles from "./styles";
 
+import {resolvers, typeDefs} from './resolvers';
+import Login from './pages/login';
+
 const cache = new InMemoryCache();
 const link = new HttpLink({uri: "http://localhost:4000/", headers: {authorization: localStorage.getItem('token')}});
-const client = new ApolloClient({cache, link});
+const client = new ApolloClient({cache, link, typeDefs, resolvers});
 
+//@client = local data query via ApolloClient class
+const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn{
+        isLoggedIn @client
+    }
+`;
+
+function IsLoggedIn(){
+    const {data} = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages/> : <Login/>;
+}
 
 cache.writeData({
     data:{
@@ -35,7 +49,7 @@ cache.writeData({
 injectStyles();
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Pages/>
+        <IsLoggedIn/>
     </ApolloProvider>,
     document.getElementById("root")
 );
